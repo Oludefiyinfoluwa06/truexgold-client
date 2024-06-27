3f195cf1eab736ab27488f05bf90b7e80cb27ec3
Dadieng <alfreddadieng18@outlook.com>
2024-06-27 01:24:33 +0100

    wallet connect

--- src/components/ConnectBtn.jsx
+++ src/components/ConnectBtn.jsx
@@ -1,25 +1,61 @@
 import React from 'react';
 import '../assets/styles.css';
+import Logo from '../assets/img/truexLogo.png'
 import { FaWallet } from "react-icons/fa6";
+import { createWeb3Modal, useWeb3Modal, defaultConfig } from '@web3modal/ethers/react';
+
+// 1. Get projectId
+const projectId = 'a629f86d61ae80ae64fab0196c035ac2'
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'TON',
+  currency: 'TON',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+// 3. Create a metadata object
+const metadata = {
+  name: 'truexgold',
+  description: 'Telegram mining bot',
+  url: 'https://truexgold.vercel.app/', // origin must match your domain & subdomain
+  icons: [<Logo />]
+}
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: '...', // used for the Coinbase SDK
+  defaultChainId: 1 // used for the Coinbase SDK
+})
+
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
 
 
 
-
-const ConnectBtn = () => {
+export default function ConnectButton() {
+  const { open } = useWeb3Modal()
   return (
-    // <div>
-    //     <a className='ConnBtn'>
-    //       <FaWallet />
-    //       <p>Connect</p>
-    //     </a>
-    // </div>
+    
     <span className='CupBtn'>
-        <button className='innerBtn'>
+        <button className='innerBtn' onClick={() => open({ view: 'Networks' })}>
             <FaWallet size={10} />
             <p>Wallet</p>
         </button>
     </span>
   )
 }
-
-export default ConnectBtn
